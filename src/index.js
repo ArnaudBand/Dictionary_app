2718058075342170b8ed7c5c3528a6a6bc042f60
Arnaud <bandonkeyea@gmail.com>
2022-05-27 17:48:23 -0600

    Correct Linters errors #1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 import './style.css';
+
+// const wrapper = document.querySelector('.wrapper');
+const inputText = document.querySelector('input');
+const infoText = document.querySelector('.info-text');
+
+const displayUI = (result, word) => {
+    
+};
+
+const fetchData = async (word) => {
+  infoText.innerHTML = `The meaning of ${word}`;
+  const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
+  const response = await fetch(url);
+  const res = await response.json();
+  displayUI(res, word);
+};
+
+inputText.addEventListener('keyup', (e) => {
+  if (e.key === 'Enter' && e.target.value) {
+    fetchData(e.target.value);
+  }
+});
