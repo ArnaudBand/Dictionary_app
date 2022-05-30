cd8ea824093e252b72d8c897b52aaeab30d38fde
Arnaud <bandonkeyea@gmail.com>
2022-05-30 07:57:25 -0600

    Add functionality #4

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
 import './style.css';
 
-// const wrapper = document.querySelector('.wrapper');
+const wrapper = document.querySelector('.wrapper');
 const inputText = document.querySelector('input');
 const infoText = document.querySelector('.info-text');
+const container = document.querySelector('.ul');
+const wordP = document.querySelector('.word p');
+const wordSpan = document.querySelector('.word span');
+const meaningsSpan = document.querySelector('.meaning span');
+const exampleSpan = document.querySelector('.example span');
+console.log(container);
 
 const displayUI = (result, word) => {
-    
+  if (result.title) {
+    infoText.innerHTML = `Can't find the meaning of <span>'${word}'</span>. Please, try another word.`;
+  } else {
+    console.log('result', result);
+    wrapper.classList.add('active');
+    // let definition = result[0].meanings[0].definitions[0];
+    wordP.innerHTML = result[0].word;
+    wordSpan.innerHTML = `${result[0].meanings[0].partOfSpeech}/${result[0].phonetic}`;
+    meaningsSpan.innerHTML = result[0].meanings[0].definitions[0].definition;
+    exampleSpan.innerHTML = result[0].meanings[0].definitions[0].example;
+  }
 };
 
 const fetchData = async (word) => {
